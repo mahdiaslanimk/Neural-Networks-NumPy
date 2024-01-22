e6746261e3dc9f7d66db1f0f933324cc19bd6de5
Mahdi Aslani <mahdiaslanimk@yahoo.com>
2024-01-22 17:04:33 +0330

    added accuracy and mae to metrics

---
+++ Metrics.py
@@ -0,0 +1,25 @@
+
+import numpy as np
+
+
+
+
+class Accuracy:
+    def __init__(self, name='accuracy', one_hot=False):
+        self.name = name
+        self.one_hot = one_hot
+
+    def compute(self, y_true, y_pred):
+        if self.one_hot:
+            y_pred_labels = np.argmax(y_pred, axis=-1)  # Convert one-hot to labels
+            correct_predictions = np.sum(y_true == y_pred_labels)
+        total_samples = len(y_true)
+        return correct_predictions / total_samples
+
+
+class MeanAbsoluteError:
+    def __init__(self, name='mae'):
+        self.name = name
+
+    def compute(self, y_true, y_pred):
+        return np.mean(np.abs(y_true - y_pred))
