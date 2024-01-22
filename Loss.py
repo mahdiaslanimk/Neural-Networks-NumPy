c016715506ca9ec02c440d77a013f75ac6779032
Mahdi Aslani <mahdiaslanimk@yahoo.com>
2024-01-22 17:02:05 +0330

    added MSE to Loss

---
+++ Loss.py
@@ -0,0 +1,16 @@
+
+import numpy as np
+
+
+
+
+class MeanSquaredError:
+    def __init__(self):
+        pass
+
+    def compute_loss(self, y_true, y_pred):
+        return np.mean((y_true - y_pred) ** 2)
+
+    def compute_derivative(self, y_true, y_pred):
+        n = y_true.shape[0]
+        return (2 / n) * (y_pred - y_true)
