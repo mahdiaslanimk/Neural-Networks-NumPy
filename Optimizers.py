5e8d3d77627b29ffb89aae7875d9acece3d86edf
Mahdi Aslani <mahdiaslanimk@yahoo.com>
2024-01-22 17:05:03 +0330

    added SGD to optimizers

---
+++ Optimizers.py
@@ -0,0 +1,13 @@
+
+import numpy as np
+
+
+
+
+class SGD:
+    def __init__(self, learning_rate):
+        self.learning_rate = learning_rate
+
+    def update(self, params, gradients):
+        for param, grad in zip(params, gradients):
+            param -= self.learning_rate * grad
