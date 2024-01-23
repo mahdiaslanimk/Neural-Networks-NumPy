2ba50c417a5db1e0dc431f41a95aaee8ebef0963
mahdiaslanimk <mahdiaslanimk@yahoo.com>
2024-01-23 15:48:11 +0330

    added Adam to Optimizers

--- Optimizers.py
+++ Optimizers.py
@@ -25,3 +25,30 @@ class MomentumSGD:
                 self.momentum * self.velocity[i] + (1 - self.momentum) * gradients[i]
             )
             params[i] -= self.learning_rate * self.velocity[i]
+
+
+class Adam:
+    def __init__(self, learning_rate, beta1=0.9, beta2=0.999, epsilon=1e-8):
+        self.learning_rate = learning_rate
+        self.beta1 = beta1
+        self.beta2 = beta2
+        self.epsilon = epsilon
+        self.m = None
+        self.v = None
+        self.t = 0
+
+    def update(self, params, gradients):
+        if self.m is None:
+            self.m = [np.zeros_like(param) for param in params]
+            self.v = [np.zeros_like(param) for param in params]
+
+        self.t += 1
+
+        for i in range(len(params)):
+            self.m[i] = self.beta1 * self.m[i] + (1 - self.beta1) * gradients[i]
+            self.v[i] = self.beta2 * self.v[i] + (1 - self.beta2) * (gradients[i] ** 2)
+
+            m_hat = self.m[i] / (1 - self.beta1**self.t)
+            v_hat = self.v[i] / (1 - self.beta2**self.t)
+
+            params[i] -= self.learning_rate * m_hat / (np.sqrt(v_hat) + self.epsilon)
