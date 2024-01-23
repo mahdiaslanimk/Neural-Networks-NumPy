4ba3e71ead5f881d3913f656cb8939c9a834c357
mahdiaslanimk <mahdiaslanimk@yahoo.com>
2024-01-23 18:43:30 +0330

    added VLBP to Optimizers
    VLBP: Variable Learning rate Backpropagation

--- Optimizers.py
+++ Optimizers.py
@@ -27,6 +27,67 @@ class MomentumSGD:
             params[i] -= self.learning_rate * self.velocity[i]
 
 
+class VLBP:
+    def __init__(
+        self,
+        model,
+        x_train,
+        y_train,
+        learning_rate,
+        momentum=None,
+        xi=0.01,
+        rho=0.9,
+        eta=1.2,
+    ):
+        self.learning_rate = learning_rate
+        self.momentum = momentum
+        self.xi = xi
+        self.rho = rho
+        self.eta = eta
+        self.optimizer = (
+            MomentumSGD(learning_rate=self.learning_rate, momentum=self.momentum)
+            if momentum
+            else SGD(learning_rate=self.learning_rate)
+        )
+        self.model = model
+        self.prev_squared_error = -np.inf
+        self.x_train = x_train
+        self.y_train = y_train
+
+    def update(self, param, gradients):
+        self.cloned_model = self.model.clone_model()
+        self.cloned_model.update_params(self.optimizer)
+        squared_error = np.mean(
+            np.square(self.y_train - self.cloned_model.forward(self.x_train))
+        )
+        self.update_learning_rate(squared_error)
+
+    def update_learning_rate(self, squared_error):
+        # VLBP rules
+        if squared_error > (1 + self.xi) * self.prev_squared_error:
+            # Rule 1: Discard weight update
+            self.learning_rate *= self.rho
+            if self.momentum != None:  # if using momentum
+                self.momentum = 0
+        elif squared_error < self.prev_squared_error:
+            # Rule 2: Accept weight update and increase learning rate
+            self.accept_update()
+            self.learning_rate *= self.eta
+            if self.momentum == 0:
+                self.momentum = self.momentum
+        else:
+            # Rule 3: Accept weight update but keep learning rate unchanged
+            self.accept_update()
+            if self.momentum == 0:
+                self.momentum = self.momentum
+
+        self.prev_squared_error = squared_error
+
+    def accept_update(self):
+        for i, cloned_layer in enumerate(self.cloned_model.layers):
+            self.model.layers[i] = cloned_layer
+
+
 class RMSprop:
     def __init__(self, learning_rate, beta=0.9, epsilon=1e-8):
         self.learning_rate = learning_rate
