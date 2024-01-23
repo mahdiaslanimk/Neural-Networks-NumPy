96922661c1a74955ff7e86751ca21a01a07cc915
mahdiaslanimk <mahdiaslani@yahoo.com>
2024-01-23 15:41:55 +0330

    added MomentumSGD to Optimizers

--- Optimizers.py
+++ Optimizers.py
@@ -8,3 +8,20 @@ class SGD:
     def update(self, params, gradients):
         for param, grad in zip(params, gradients):
             param -= self.learning_rate * grad
+
+
+class MomentumSGD:
+    def __init__(self, learning_rate, momentum):
+        self.learning_rate = learning_rate
+        self.momentum = momentum
+        self.velocity = None
+
+    def update(self, params, gradients):
+        if self.velocity is None:
+            self.velocity = [np.zeros_like(param) for param in params]
+
+        for i in range(len(params)):
+            self.velocity[i] = (
+                self.momentum * self.velocity[i] + (1 - self.momentum) * gradients[i]
+            )
+            params[i] -= self.learning_rate * self.velocity[i]
